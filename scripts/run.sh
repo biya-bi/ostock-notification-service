78f0647b592e9a41016925abfcab6485e87cf992
Paul Muriel Biya-Bi <paulmuriel.biyabi@gmail.com>
2024-05-02 19:08:26 -0400

    Improve script file

--- scripts/run.sh
+++ scripts/run.sh
@@ -15,6 +15,11 @@ if [[ ! -f ${VAPID_PUBLIC_KEY_FILE} ]]; then
     exit 1
 fi
 
+if [[ ! -f ${VAPID_SUBJECT_FILE} ]]; then
+    echo "The file ${VAPID_SUBJECT_FILE} does not exist"
+    exit 1
+fi
+
 export JWT_ISSUER_URI=`cat ${JWT_ISSUER_URI_FILE}`
 export VAPID_PRIVATE_KEY=`cat ${VAPID_PRIVATE_KEY_FILE}`
 export VAPID_PUBLIC_KEY=`cat ${VAPID_PUBLIC_KEY_FILE}`
